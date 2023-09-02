a2729bd598aeed9ac728b9a33d933c92075addc2
Hessam <hessam.imen@gmail.com>
2023-09-03 09:14:19 +1000

    feat: implement fetching the backend data using get request

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,10 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpencesOutput from "../components/ExpencesOutput/ExpencesOutput";
 import { ExpensesContext } from "../store/expense-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
--- util/http.js
+++ util/http.js
@@ -1,8 +1,28 @@
 import axios from "axios";
 
+const BACKEND_URL =
+  "https://react-native-expense-tra-dce1f-default-rtdb.firebaseio.com";
+
 export function storeExpense(expenseData) {
-  return axios.post(
-    "https://react-native-expense-tra-dce1f-default-rtdb.firebaseio.com/expenses.json",
-    expenseData
-  );
+  return axios.post(BACKEND_URL + "/expenses.json", expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BACKEND_URL + "/expenses.json");
+
+  const expenses = [];
+
+  //   console.log("RESPONSE DATA:", response.data);
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
